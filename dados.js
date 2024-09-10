95d4ccdffe41ea14bf6e6a0aa331d2ff75174c69
Douglas Machado <87953890+DouglasVMachado@users.noreply.github.com>
2024-09-09 22:28:24 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [{
+    titulo : "God of War",
+    descricao : "God of War (2018) marca uma reinvenção da franquia, abandonando a mitologia grega e se aprofundando na nórdica. O jogo apresenta um Kratos mais maduro e introspectivo, em uma jornada de reconciliação com seu filho, Atreus.",
+    link : "https://pt.wikipedia.org/wiki/God_of_War_(2018)",
+    lancamento : "20 de abril de 2018",
+    tags : "kratos atreus deus guerra "
+}, 
+{
+    titulo : "Elden Ring",
+    descricao : "Elden Ring é um RPG de ação aclamado pela crítica, que convida os jogadores a explorar um vasto mundo de fantasia repleto de mistérios e perigos.",
+    link : "https://pt.wikipedia.org/wiki/Elden_Ring",
+    lancamento : "25 de fevereiro de 2022",
+    tags : "rpg fantasia açao"
+},
+{
+    titulo : "Monster Hunter",
+    descricao : "Monster Hunter: World é um marco na franquia, sendo o primeiro jogo da série a ser lançado para um público mais amplo. Ele apresenta um mundo aberto vibrante e cheio de criaturas gigantescas, onde os jogadores assumem o papel de caçadores, equipando-se com diversas armas e armaduras para enfrentar desafios cada vez maiores.",
+    link : "https://pt.wikipedia.org/wiki/Monster_Hunter:_World",
+    lancamento : "26 de janeiro de 2018",
+    tags : "monstros rpg mundo"
+}
+];
